01d1746ca18fcca81b03d8aee75c2f51fdf4fe4b
yan640 <yan-cr@mail.ru>
2018-03-01 15:08:16 +0300

    0103

--- sample/src/main/java/co/netguru/android/chatandroll/feature/main/video/VideoFragmentView.kt
+++ sample/src/main/java/co/netguru/android/chatandroll/feature/main/video/VideoFragmentView.kt
@@ -11,7 +11,7 @@ interface VideoFragmentView : MvpView {
     fun showPairingConfirmationDialog(device: DeviceInfoFirebase)
     //fun showPairPhones(PairedPhones : Map<String, String>)
     fun saveFirebaiseKey(key: String)
-
+    fun ShowFirebaiseKey(key: String)
     fun showStartRouletteView()
     fun disconnect()
     fun attachService()
