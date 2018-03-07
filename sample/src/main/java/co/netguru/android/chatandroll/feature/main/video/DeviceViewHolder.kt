58021e572e5d302c3581a5b3a22ec5ccfae51b25
gleb-r <rad.gleb@gmail.com>
2018-03-07 20:19:07 +0300

    adapter for paired devices recylerView added

---
+++ sample/src/main/java/co/netguru/android/chatandroll/feature/main/video/DeviceViewHolder.kt
@@ -0,0 +1,12 @@
+package co.netguru.android.chatandroll.feature.main.video
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by Gleb on 07.03.2018.
+ */
+class DeviceViewHolder(val view:View):RecyclerView.ViewHolder(view) {
+
+
+}
---
+++ sample/src/main/java/co/netguru/android/chatandroll/feature/main/video/PairedDevicesAdapter.kt
@@ -0,0 +1,22 @@
+package co.netguru.android.chatandroll.feature.main.video
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import co.netguru.android.chatandroll.data.model.PairedDevice
+
+/**
+ * Created by Gleb on 07.03.2018.
+ */
+class PairedDevicesAdapter(val devices: List<PairedDevice>):RecyclerView.Adapter<DeviceViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): DeviceViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: DeviceViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
